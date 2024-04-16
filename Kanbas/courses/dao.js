fb0b1457cc22371d37dc123a1aeb98edc0778546
yuezhang23 <jackiezy1002@gmail.com>
2024-04-15 20:52:02 -0400

    replace _id with id

--- Kanbas/courses/dao.js
+++ Kanbas/courses/dao.js
@@ -9,5 +9,5 @@ export const findAllCourses = () => model.CourseModel.find();
 export const findCourseById = (cId) => model.CourseModel.findOne({cid : cId});
 
 export const updateCourse = (cId, course) =>  model.CourseModel.updateOne({ cid: cId }, { $set: course });
-export const deleteCourse = (cId) => model.CourseModel.deleteOne({ cid: cId });
+export const deleteCourse = (id) => model.CourseModel.deleteOne({ _id: id });
 
--- Kanbas/modules/model.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-import schema from "./schema.js";
-const model = mongoose.model("moduleModel", schema);
-export default model;
